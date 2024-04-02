573102ff327a8fe78f645774804571de63c885ac
Ichrafsassi <ichraf.sassi.456@gmail.com>
2024-04-02 17:07:47 +0100

    debug

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,34 +2,32 @@ import prismadb from "@/lib/prismadb";
 import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
-export default async function DashboardLayout({
-    children,
-    params
+
+export default async function SetupLayout({
+    children
 }: {
     children: React.ReactNode;
-    params: { storeId: string };
 }) {
     const { userId } = auth();
 
-    if ( !userId ) {
+    if (!userId) {
         redirect("/sign-in");
     }
 
     const store = await prismadb.store.findFirst({
         where: {
-            id: params.storeId,
             userId
         }
-    })
+    });
 
-    if ( !store ) {
-        redirect("/");
+    if (store) {
+        redirect(`/${store.id}`);
     }
 
     return (
         <>
-        <div>This will be Navbar</div>
-        {children}
+         {children}
         </>
     );
+
 };
