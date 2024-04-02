f740d2101ead423b054b31d730c0b50ef949ac82
Ichrafsassi <ichraf.sassi.456@gmail.com>
2024-04-02 20:42:52 +0100

    Navbar-admin

--- components/modals/store-modal.tsx
+++ components/modals/store-modal.tsx
@@ -39,8 +39,9 @@ export const StoreModal = () => {
 
             const response = await axios.post("/api/stores", values);
 
-            console.log(response.data);
-            toast.success("Store created.");
+            window.location.assign(`/${response.data.id}`); // Redirect to the store page
+
+            // console.log(response.data);
         } catch (error) {
             toast.error("Something went wrong.");
         } finally {
