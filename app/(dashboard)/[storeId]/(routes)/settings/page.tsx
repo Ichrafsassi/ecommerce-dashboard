fa3bced80ea13ddf4446f580399da63a1b8a5f83
Ichrafsassi <ichraf.sassi.456@gmail.com>
2024-04-03 04:36:36 +0100

    billbord+hook+components

--- app/(dashboard)/[storeId]/(routes)/settings/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/page.tsx
@@ -27,7 +27,7 @@ const SettingsPage = async ({
     redirect('/');
   }
 
-  return (
+  return ( 
     <div className="flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
         <SettingsForm initialData={store} />
